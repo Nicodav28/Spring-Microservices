8318e04bbd279eb4659d6071c1e587500e60fcda
Nicodav28 <esteveznicolas0@gmail.com>
2024-10-07 18:44:02 -0500

    cambia estructura del proyecto

--- microservices-parent/product-service/src/main/java/com/dev/nicodav28/product_service/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dev.nicodav28.product_service.repository;
-
-import com.dev.nicodav28.product_service.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product, String> {
-}
