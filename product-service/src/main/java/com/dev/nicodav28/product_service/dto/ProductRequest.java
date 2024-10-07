353c75d1c015e21a793ead8afdb87eb1d7b5ae43
Nicodav28 <esteveznicolas0@gmail.com>
2024-10-07 00:04:16 -0500

    ✨ (ProductController.java): Crear controlador para productos ✨ (ProductRequest.java): Crear DTO para solicitudes de productos ✨ (ProductResponse.java): Crear DTO para respuestas de productos ✨ (Product.java): Crear modelo de Producto ✨ (ProductRepository.java): Crear repositorio para productos ✨ (ProductService.java): Crear servicio para productos
    Se han añadido nuevos archivos y clases para implementar la funcionalidad de gestión de productos en el servicio. Se han creado el controlador, los DTO para las solicitudes y respuestas, el modelo de Producto, el repositorio y el servicio para manejar las operaciones relacionadas con los productos.

--- product-service/src/main/java/com/dev/nicodav28/product_service/dto/ProductRequest.java
+++ product-service/src/main/java/com/dev/nicodav28/product_service/dto/ProductRequest.java
@@ -1,22 +1,17 @@
-package com.dev.nicodav28.product_service.model;
+package com.dev.nicodav28.product_service.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
 
-@Document(value = "product")
+@Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Builder
-@Data
-public class Product {
-    @Id
-    private String id;
+public class ProductRequest {
     private String name;
     private String description;
     private BigDecimal price;
